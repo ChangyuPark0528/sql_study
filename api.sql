80a9a1fe4097dca62954a66d76e1b20960b9b82e
Changyu <changyu1061@naver.com>
2023-10-26 10:40:08 +0900

    1026 쿼리추가

--- api.sql
+++ api.sql
@@ -18,4 +18,12 @@ SELECT * FROM
         AND area2 LIKE '%서구%'
     )
 WHERE ROWNUM = 1;
+-----------------------------------------------*위도 경도 구하기
+SELECT * FROM
+    (
+        SELECT * FROM test_location
+        ORDER BY ABS(latitude - 37.5621181) + ABS(longitude -126.9428028)
+    )
+WHERE ROWNUM = 1;
 
+SELECT * FROM test_location;
